21aec315695fd7daaeafabbc3ab257763f5d995e
pradnyak9 <pradnyakhairnar9@gmail.com>
2020-02-13 09:26:38 +1030

    commit product add operation

---
+++ product-cart/src/api/models/Product.js
@@ -0,0 +1,22 @@
+// Product.js
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define collection and schema for product
+
+let Product = new Schema({
+    ProductName: {
+        type: String
+    },
+    ProductDescription: {
+        type: String
+    },
+    ProductPrice: {
+        type: Number
+    }
+}, {
+    collection: 'Product'
+});
+
+module.exports = mongoose.model('Product', Product);
--- product-cart/src/app/app.module.ts
+++ product-cart/src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductAddComponent } from './product-add/product-add.component';
 import { ProductGetComponent } from './product-get/product-get.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductsService } from './products.service';
 
 @NgModule({
   declarations: [
@@ -16,9 +20,13 @@ import { ProductEditComponent } from './product-edit/product-edit.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SlimLoadingBarModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ ProductsService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
